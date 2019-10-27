5d995affd7031ed1c198abd69e7f44ab9a8a1470
Leena Elzeiny <leena.elzeiny@gmail.com>
2019-10-27 06:48:06 -0700

    database intergration

--- landmark-web/src/js/Mapbox.js
+++ landmark-web/src/js/Mapbox.js
@@ -1,4 +1,5 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoia2Zlc2xlciIsImEiOiJjazI3cG9jNTcwZ3VoM21sc3lhOHFtNnYzIn0.i6MLXYxFPfw96PdwMfIWnw';
+secretToken = 'sk.eyJ1IjoibGVsemVpbnkiLCJhIjoiY2syOHZoZnlqMGV5bDNjbnI5ejllNnFzNyJ9.zcLcywC3GojHe1zvw_N23Q'
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/dark-v10',
@@ -10,23 +11,17 @@ var map = new mapboxgl.Map({
 function submitForm(){
     var address = document.getElementById("address").value;
 
-    const Http = new XMLHttpRequest();
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ serialize(address) +'.json?types=address&access_token='+ mapboxgl.accessToken;
-    Http.open("GET",url);
-    Http.send();
-    Http.onreadystatechange = (e) => {
-        // call whatever function to update the map with new data
-        // sample
-
-        console.log( serialize(address) );
-        console.log(mapboxgl.accessToken);
-        console.log("***");
-        console.log(Http.responseText);
-        var x = 0;
-        x = JSON.parse('{"type":"FeatureCollection","query":["santa"],"features":[{"id":"address.2777127089908054","type":"Feature","place_type":["address"],"relevance":1,"properties":{"accuracy":"street"},"text":"Santa Rosa Boulevard","place_name":"Santa Rosa Boulevard, Doncaster East Victoria 3109, Australia","center":[145.178146,-37.765812],"geometry":{"type":"Point","coordinates":[145.178146,-37.765812]},"context":[{"id":"locality.5630161037312860","wikidata":"Q5295495","text":"Doncaster East"},{"id":"postcode.12635326254513450","text":"3109"},{"id":"place.7068896531111320","wikidata":"Q3141","text":"Melbourne"},{"id":"region.10354338764038050","short_code":"AU-VIC","wikidata":"Q36687","text":"Victoria"},{"id":"country.3050683007346070","short_code":"au","wikidata":"Q408","text":"Australia"}]},{"id":"address.401651220436184","type":"Feature","place_type":["address"],"relevance":1,"properties":{"accuracy":"street"},"text":"Santarosa Street","place_name":"Santarosa Street, Colebee New South Wales 2761, Australia","center":[150.8571796,-33.7244547],"geometry":{"type":"Point","coordinates":[150.8571796,-33.7244547]},"context":[{"id":"locality.14750822858169760","text":"Colebee"},{"id":"postcode.9816177759266640","text":"2761"},{"id":"place.4960085988742460","wikidata":"Q3130","text":"Sydney"},{"id":"region.10849457265831280","short_code":"AU-NSW","wikidata":"Q3224","text":"New South Wales"},{"id":"country.3050683007346070","short_code":"au","wikidata":"Q408","text":"Australia"}]},{"id":"address.2902233331191242","type":"Feature","place_type":["address"],"relevance":1,"properties":{"accuracy":"street"},"text":"Santarosa Avenue","place_name":"Santarosa Avenue, Tarneit Victoria 3029, Australia","center":[144.6490093,-37.8467266],"geometry":{"type":"Point","coordinates":[144.6490093,-37.8467266]},"context":[{"id":"locality.16465363026367850","wikidata":"Q7686407","text":"Tarneit"},{"id":"postcode.13063936157867600","text":"3029"},{"id":"place.7068896531111320","wikidata":"Q3141","text":"Melbourne"},{"id":"region.10354338764038050","short_code":"AU-VIC","wikidata":"Q36687","text":"Victoria"},{"id":"country.3050683007346070","short_code":"au","wikidata":"Q408","text":"Australia"}]},{"id":"address.4747843698089450","type":"Feature","place_type":["address"],"relevance":1,"properties":{"accuracy":"street"},"text":"Santa Catalina Crescent","place_name":"Santa Catalina Crescent, Seaford Rise South Australia 5169, Australia","center":[138.4824468,-35.2006796],"geometry":{"type":"Point","coordinates":[138.4824468,-35.2006796]},"context":[{"id":"locality.17908285661520300","wikidata":"Q7440389","text":"Seaford Rise"},{"id":"postcode.7017836284635270","text":"5169"},{"id":"place.10281734113062160","wikidata":"Q5112","text":"Adelaide"},{"id":"region.10227158564642850","short_code":"AU-SA","wikidata":"Q35715","text":"South Australia"},{"id":"country.3050683007346070","short_code":"au","wikidata":"Q408","text":"Australia"}]},{"id":"address.6569340582465604","type":"Feature","place_type":["address"],"relevance":1,"properties":{"accuracy":"street"},"text":"Santa Cruz View","place_name":"Santa Cruz View, Point Cook Victoria 3030, Australia","center":[144.7434822,-37.8950444],"geometry":{"type":"Point","coordinates":[144.7434822,-37.8950444]},"context":[{"id":"locality.4307885032368650","wikidata":"Q7207994","text":"Point Cook"},{"id":"postcode.5120185980658060","text":"3030"},{"id":"place.7068896531111320","wikidata":"Q3141","text":"Melbourne"},{"id":"region.10354338764038050","short_code":"AU-VIC","wikidata":"Q36687","text":"Victoria"},{"id":"country.3050683007346070","short_code":"au","wikidata":"Q408","text":"Australia"}]}],"attribution":"NOTICE: © 2019 Mapbox and its suppliers. All rights reserved. Use of this data is subject to the Mapbox Terms of Service (https://www.mapbox.com/about/maps/). This response and the information it contains may not be retained. POI(s) provided by Foursquare."}');
-        console.log(x.features[0].geometry.coordinates);
-    }
-
+    /*const Http = new XMLHttpRequest();
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ serialize(address) +'.json?types=address&access_token='+ secretToken;
+    
+    fetch(url).then(function(response) {
+        response.text().then(function(text) {
+            var x= JSON.parse(text);
+            console.log(x.features[0].geometry.coordinates);
+        });
+    });*/
+    
+    
     var xhr = new XMLHttpRequest();
     xhr.open("POST",'/submit', true);
     xhr.setRequestHeader('Content-Type', 'application/json');
@@ -40,19 +35,13 @@ function submitForm(){
             rating: 3.5,
             type: 1
     }));
-    // axios.post('/submit',
-    //     {
-    //         'latitude': 50,
-    //         'longitude': 100,
-    //         'name': document.getElementById("name").value,
-    //         'description': document.getElementById("description").value,
-    //         'image': null,
-    //         'n-rating': 32,
-    //         'rating': 3.5,
-    //         'type': 1
-    // })
-    // .then(
-    // )
+
+    
+    menu = document.getElementById("menu");
+    popup = document.getElementById("addStop");
+    popup.style.display = "none";
+    menu.style.display = "flex";
+    getPoints();
 }
 function serialize(str){
     str = str.toString().split(" ");
@@ -68,15 +57,32 @@ function serialize(str){
 function getPoints(){
     const Http = new XMLHttpRequest();
     const url = '/points';
-    Http.open("GET",url);
-    Http.send();
-
-    Http.onreadystatechange = (e) => {
-        // call whatever function to update the map with new data
-        // sample
-        console.log(Http.responseText);
-    }
+    fetch(url).then(function(response) {
+        response.text().then(function(text) {
+          console.log(text);
+          var x = JSON.parse(text);
+          for(var j = 0; j < x.length; j++){
+            var card = document.createElement('div');
+            card.className = 'locationCard';
+    
+            var name = document.createElement('h3');
+            name.innerHTML = x[j].name;
+            for (var i = 0; i < 5; i++){
+                var rating = document.createElement('i');
+                rating.className = "fa fa-star";
+                name.appendChild(rating);
+            }
+            card.appendChild(name);
+    
+            var description = document.createElement('p');
+            description.innerHTML = x[j].description;
+            card.appendChild(description);
+            document.getElementById('exploreContainer').appendChild(card);
+        }
+        });
+      });
 }
+
 
 map.on('load', function() {
     // Add a new source from our GeoJSON data and set the
--- landmark-web/src/js/popup.js
+++ landmark-web/src/js/popup.js
@@ -14,7 +14,6 @@ function toggleAction(action){
 function myMove() {
     var elem = document.getElementById("explore");
     var set_width = window.innerWidth * 0.33;
-  console.log(elem.style.left);
     if(elem.style.left == ""){
         elem.style.display = "block";
         var pos = -1 * set_width;
@@ -34,3 +33,45 @@ function myMove() {
         elem.style.display = "none";
     }
   }
+
+  
+var geojson = {
+  type: 'FeatureCollection',
+  features: [{
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-77.032, 38.913]
+    },
+    properties: {
+      title: 'Mapbox',
+      description: 'Washington, D.C.'
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-122.414, 37.776]
+    },
+    properties: {
+      title: 'Mapbox',
+      description: 'San Francisco, California'
+    }
+  }]
+};
+
+// add markers to map
+geojson.features.forEach(function(marker) {
+
+  // create a HTML element for each feature
+  var el = document.createElement('div');
+  el.className = 'marker';
+
+  // make a marker for each feature and add to the map
+  new mapboxgl.Marker(el)
+    .setLngLat(marker.geometry.coordinates)
+    .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+    .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
+    .addTo(map);
+});
